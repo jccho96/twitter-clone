d6c4f905380bc46b097b5968449e767f62ae8808
jccho96 <joanne.yy.cho@gmail.com>
2022-01-24 03:40:53 -0800

    sidebar, darkmode

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ const newsfeedPage = document.querySelector(".feeds-page");
 const loginModal = document.querySelector(".login-modal");
 const modalX = document.querySelector(".login-modal i");
 const loginFormBtn = document.querySelector(".login-form-btn");
+const postBtn = document.querySelector(".post-btn");
+const modalWrapper = document.querySelector(".modal-wrapper");
+const modal = document.querySelector(".modal");
+const postModalX = document.querySelector(".modal-header i");
+const modalPostBtn = document.querySelector(".modal-header button");
+const modalFooerPlus = document.querySelector(".modal-footer span");
+const modalInput = document.querySelector(".modal-input");
+const user = document.querySelector(".user");
+const sidebar = document.querySelector(".sidebar");
+const sidebarWrapper = document.querySelector(".sidebar-wrapper");
+const xBtn = document.querySelector(".sidebar-header i");
+const toggle = document.querySelector(".toggle");
+const circle = document.querySelector(".circle");
+
 // Main Page
 const goToLoginPage = () => {
   mainPage.style.display = "none";
@@ -47,4 +61,54 @@ loginFormBtn.addEventListener("click", () => {
   } else {
     loginModal.style.display = "block";
   }
+});
+
+// Newsfeed page
+// Post modal
+postBtn.addEventListener("click", () => {
+  modal.style.display = "block";
+  modalWrapper.classList.add("modal-wrapper-display");
+});
+
+const changeOpacity = (x) => {
+  modalPostBtn.style.opacity = x;
+  modalFooerPlus.style.opacity = x;
+};
+
+postModalX.addEventListener("click", () => {
+  modal.style.display = "none";
+  modalWrapper.classList.remove("modal-wrapper-display");
+
+  if (modalInput.value !== "") {
+    modalInput.value = "";
+    changeOpacity(0.5);
+  }
+});
+
+modalInput.addEventListener("keydown", (e) => {
+  if (e.target.value !== "") {
+    changeOpacity(1);
+  }
+});
+
+modalInput.addEventListener("blur", (e) => {
+  if (e.target.value === "") {
+    changeOpacity(0.5);
+  }
+});
+
+// Sidebar
+user.addEventListener("click", () => {
+  sidebar.classList.add("sidebar-display");
+  sidebarWrapper.classList.add("sidebar-wrapper-display");
+});
+
+xBtn.addEventListener("click", () => {
+  sidebar.classList.remove("sidebar-display");
+  sidebarWrapper.classList.remove("sidebar-wrapper-display");
+});
+
+// Dark mode
+toggle.addEventListener("click", () => {
+  circle.classList.toggle("move");
 });
